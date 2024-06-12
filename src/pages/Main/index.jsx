983ad633e82f1ba08038e2f9d0c359423f9453a8
Lorena <77021882+loresgarcia@users.noreply.github.com>
2024-06-12 16:07:49 -0300

    fix: cards display on page main

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import Card from "../../components/Card"
 
+import "./Main.css"
+
 const Main = () => {
     const [cards, setCards] = useState(
         [
@@ -10,6 +12,18 @@ const Main = () => {
             }, {
                 id: 2,
                 message: "card 2"
+            }, {
+                id: 3,
+                message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+            }, {
+                id: 4,
+                message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            }, {
+                id: 5,
+                message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+            }, {
+                id: 6,
+                message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             }
         ]
     );
@@ -21,19 +35,21 @@ const Main = () => {
 
     return (
         <>
-            {cards.length > 0 
-                ? cards.map(c => 
-                    <Card 
-                        key={c.id} 
-                        id={c.id} 
-                        message={c.message} 
-                        handleDelete={handleDelete} 
-                        cards={cards}
-                        setCards={setCards}
-                    />
-                )
-                : <></>
-            }
+            <section className="cards">
+                {cards.length > 0 
+                    ? cards.map(c => 
+                        <Card 
+                            key={c.id} 
+                            id={c.id} 
+                            message={c.message} 
+                            handleDelete={handleDelete} 
+                            cards={cards}
+                            setCards={setCards}
+                        />
+                    )
+                    : <></>
+                }
+            </section>
         </>
     )
 }
